e89175b660b84b2eeb1c2416f6cd74d5238968b0
Ivan Matveev <evanmatveev@gmail.com>
2018-09-03 18:50:40 +0300

    Add new contacts to the app functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,13 @@ class App extends Component {
   state = {
     contacts: []
   };
+  createContact = contact => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(state => ({
+        contacts: state.contacts.concat({ contact })
+      }));
+    });
+  };
   removeContact = contact => {
     this.setState(state => ({
       contacts: state.contacts.filter(c => c.id !== contact.id)
@@ -32,7 +39,17 @@ class App extends Component {
             />
           )}
         />
-        <Route path="/create" component={CreateContact} />
+        <Route
+          path="/create"
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import ImageInput from "./ImageInput";
+import serializeForm from "form-serialize";
 
 class CreateContact extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const values = serializeForm(e.target, { hash: true });
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values);
+    }
+  };
   render() {
-    return <h2>Create contact</h2>;
+    return (
+      <div>
+        <Link to="/" className="close-create-contact" />
+        <form onSubmit={this.handleSubmit} className="create-contact-form">
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            max-height={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="name" placeholder="Email" />
+            <button>Add button</button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
